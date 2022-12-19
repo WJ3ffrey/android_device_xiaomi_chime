993baab9a97f077bdd5cec155880ea78d23fbe11
WJ3ffrey <121066198+WJ3ffrey@users.noreply.github.com>
2022-12-20 00:00:02 +0100

    Set WITH_GMS to true
    This will include the proper makefiles under 'vendor/partner_gms/products/' and build the desired packages

--- lineage_chime.mk
+++ lineage_chime.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/xiaomi/chime/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# MicroG/GApps
+WITH_GMS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_chime
 PRODUCT_DEVICE := chime
