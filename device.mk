fc97ca7ef99d3e8c76b1f0ea10787e53d7495afc
WJ3ffrey <121066198+WJ3ffrey@users.noreply.github.com>
2022-12-24 06:06:32 +0100

    chime: Copy system_ext public.libraries list
    probably redundant

--- device.mk
+++ device.mk
@@ -375,7 +375,8 @@ PRODUCT_PACKAGES += \
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
+    $(LOCAL_PATH)/configs/public.libraries-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
 
 # QMI
 PRODUCT_PACKAGES += \
